d9515be9a094a3cd75bd4f09613b8cee32b6ac5e
rchelanggie <rachelpasorong@gmail.com>
2024-07-02 10:31:21 +0700

    Flutter

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/aaliyahfarah/Desktop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/aaliyahfarah/Documents/Code/PPB/codelabs-main/boring_to_beautiful/final"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/aaliyahfarah/Documents/Code/PPB/codelabs-main/boring_to_beautiful/final/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1.0.0"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9lZGQ4NTQ2MTE2NDU3YmRmMWM1YmRmYjEzZWNiOTQ2M2QyYmI1ZWQ0Lw=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/aaliyahfarah/Documents/Code/PPB/codelabs-main/boring_to_beautiful/final/.dart_tool/package_config.json"
